3645ccfdedb0f071a319f97f30ce51c5559bce2b
Tejaswi Patil <teju.patil2410@gmail.com>
2026-02-05 23:37:44 +0530

    test: verify frontend CD auto deployment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,14 @@ import RegistrationForm from "./components/RegistrationForm";
 function App() {
   return (
     <div>
-      <h1>User Registration</h1>
+      <h1>User Registration - Final Verification</h1>
       <RegistrationForm />
+      <h1 style={{ color: "green" }}>
+  🚀 Frontend Auto-Deployed via GitHub Actions
+      </h1>
+
     </div>
+
   );
 }
 
